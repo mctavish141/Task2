3fbb9d3e4022ee639c72be927711a3bc669c0686
Serhii Kopach <Serega@MacBook-Air.local>
2015-09-26 15:39:35 +0300

    v1.0

--- Task2/main.c
+++ Task2/main.c
@@ -8,8 +8,50 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main () {
+    printf ("Enter city code: ");
+    int code;
+    scanf ("%d", &code);
+    
+    printf ("Enter hours: ");
+    int hours;
+    scanf ("%d", &hours);
+    
+    printf ("Enter minutes: ");
+    int minutes;
+    scanf ("%d", &minutes);
+    
+    double tariff;
+    switch (code) {
+        case 44: {
+            tariff = 0.44;
+            break;
+        }
+        case 37: {
+            tariff = 1.05;
+            break;
+        }
+        case 111: {
+            tariff = 0.1;
+            break;
+        }
+        case 1: {
+            tariff = 30;
+            break;
+        }
+        case 66: {
+            tariff = 0;
+            break;
+        }
+        default: {
+            tariff = 1;
+            break;
+        }
+    }
+    
+    int allMinutes = (hours * 60) + minutes;
+    double resultTariff = allMinutes * tariff;
+    printf ("%.2lf\n", resultTariff);
+    
     return 0;
 }
